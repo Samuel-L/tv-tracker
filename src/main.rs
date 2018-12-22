b2100fafd6c0dca5a6be1a6a6bf26218ebe352c4
Samuel L <samuell.person@gmail.com>
2018-12-22 20:43:01 +0100

    add: store_series function

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ mod tvfile;
 
 fn main() {
     tvfile::create_tvfile(tvfile::FILENAME);
+
+    tvfile::store_series(tvfile::FILENAME, 82, "Game of Thrones", "2017-08-27");
 }
--- src/tvfile/mod.rs
+++ src/tvfile/mod.rs
@@ -1,19 +1,22 @@
-use std::fs;
-use std::path::Path;
 use std::error::Error;
-use std::io::Write;
+use std::fs;
 use std::io::Read;
+use std::io::Write;
+use std::path::Path;
 
 /// The default filename for the `tvfile`
 pub static FILENAME: &str = ".tvfile.csv";
 
 // `csv` headers for the `tvfile`
-static FILE_HEADERS: &str = "maze_id, title, latest";
+static FILE_HEADERS: &str = "maze_id, title, latest\n";
 
-/// Returns a created `.tvfile.csv` file
+/// Creates a `.tvfile.csv` file
 /// # Arguments
 /// * `filename` - A string containing the filename
-/// 
+///
+/// # Returns
+/// The creates `.tvfile.csv` file
+///
 /// # Example usage
 /// ```
 /// tvfile::create_tvfile(tvfile::FILENAME);
@@ -29,17 +32,35 @@ pub fn create_tvfile(filename: &str) -> fs::File {
                 display,
                 error.description()
             );
-        }  
+        }
     };
     match file.write_all(FILE_HEADERS.as_bytes()) {
         Ok(_) => return file,
-        Err(error) => {
-            panic!(
-                "There was a problem writing to the file {}: {}",
-                display,
-                error.description()
-            )
-        }
+        Err(error) => panic!(
+            "There was a problem writing to the file {}: {}",
+            display,
+            error.description()
+        ),
+    }
+}
+
+/// Appends a new tv series to be tracked
+/// # Arguments
+/// * `filename` - A string containing the filename
+/// * `maze_id` - The ID of the series on the TVMaze API
+/// * `title` - The series title
+/// * `latest` - The airdate of the latest episode
+///
+/// # Example usage
+/// ```
+/// tvfile::store_series(tvfile::FILENAME, 82, "Game of Thrones", "2017-08-27");
+/// ```
+pub fn store_series(filename: &str, maze_id: i32, title: &str, latest: &str) {
+    let series_string = format!("{}, {}, {}", maze_id, title, latest);
+
+    let mut file = fs::OpenOptions::new().append(true).open(filename).unwrap();
+    if let Err(e) = writeln!(file, "{}", series_string) {
+        eprintln!("Couldn't write to file: {}", e);
     }
 }
 
@@ -64,19 +85,58 @@ mod create_tvfile_tests {
 
         let mut tvfile = fs::File::open(filename).expect("Unable to open file.");
         let mut tvfile_content = String::new();
-        tvfile.read_to_string(&mut tvfile_content).expect("Unable to read file.");
+        tvfile
+            .read_to_string(&mut tvfile_content)
+            .expect("Unable to read file.");
 
         assert_eq!(tvfile_content, FILE_HEADERS);
 
         tear_down(filename);
     }
+}
 
-    fn tear_down(filename: &str) -> () {
-        match fs::remove_file(filename) {
-            Ok(success) => return success,
-            Err(error) => {
-                panic!(error);
-            }
+#[cfg(test)]
+mod store_series_tests {
+    use super::*;
+
+    #[test]
+    fn test_appends_tracker_string() {
+        let test_file = "test_file.csv";
+        let series_string = "1, title, latest\n";
+        create_test_file(test_file);
+
+        store_series("test_file.csv", 1, "title", "latest");
+        let mut file = fs::File::open(test_file).expect("Unable to open file.");
+        let mut file_content = String::new();
+        file.read_to_string(&mut file_content)
+            .expect("Unable to read file.");
+
+        assert_eq!(file_content, series_string);
+
+        tear_down(test_file)
+    }
+}
+
+fn create_test_file(filename: &str) {
+    let path = Path::new(filename);
+    let display = path.display();
+    match fs::File::create(&path) {
+        Ok(file) => file,
+        Err(error) => {
+            panic!(
+                "There was a problem creating the file {}: {}",
+                display,
+                error.description()
+            );
+        }
+    };
+}
+
+fn tear_down(filename: &str) -> () {
+    match fs::remove_file(filename) {
+        Ok(success) => return success,
+        Err(error) => {
+            panic!(error);
         }
     }
 }
